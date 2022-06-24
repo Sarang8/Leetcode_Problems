62efb8670d852203a109b8c416d57e925fb0214a
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-06-24 22:20:03 +0530

    Time: 7 ms (74.88%), Space: 10.3 MB (63.48%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -2,23 +2,39 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         
-        unordered_map<int,int>m;
+//         unordered_map<int,int>m;
+//         vector<int>ans;
+//         set<int>s;
+        
+//         for(int i=0; i<nums1.size(); i++){
+//             m[nums1[i]]++;
+//         }
+        
+//         for(int i=0; i<nums2.size(); i++){
+//             if(m.find(nums2[i])!=m.end()){
+//                 s.insert(nums2[i]);
+//                 //ans.push_back(nums2[i]);
+//             }
+//         }
+        
+//         for(auto it:s){
+//             ans.push_back(it);
+//         }
+        
+//         return ans;
+        
+        unordered_set<int>s;
         vector<int>ans;
-        set<int>s;
         
         for(int i=0; i<nums1.size(); i++){
-            m[nums1[i]]++;
+            s.insert(nums1[i]);
         }
         
         for(int i=0; i<nums2.size(); i++){
-            if(m.find(nums2[i])!=m.end()){
-                s.insert(nums2[i]);
-                //ans.push_back(nums2[i]);
-            }
-        }
-        
-        for(auto it:s){
-            ans.push_back(it);
+            if(s.count(nums2[i])){
+                ans.push_back(nums2[i]);
+                s.erase(nums2[i]);
+            }   
         }
         
         return ans;
