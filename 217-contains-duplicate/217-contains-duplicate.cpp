e081b1244f3bf5f8de0bbb566f4d7a248a2edccf
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-06-24 11:08:41 +0530

    Time: 256 ms (5.03%), Space: 54.9 MB (6.23%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        map<int, int>m;
+        
+        for(int i=0; i<n; i++){
+            m[nums[i]]++;
+        }
+        
+        for(auto it:m){
+            if(it.second>=2)
+                    return true;
+           // m.erase(it);
+        }
+        return false;
+        
+    }
+};
