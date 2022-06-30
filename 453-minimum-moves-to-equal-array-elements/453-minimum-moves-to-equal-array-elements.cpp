51938afcf23909e3951601380fcf910d1a631183
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-06-30 10:01:28 +0530

    Time: 62 ms (50.93%), Space: 28.3 MB (41.83%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
-    int minMoves(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n=nums.size();
-        int ans=0;
+    int minMoves2(vector<int>& nums) {
         
-        for(int i=1; i<n; i++){
-            ans+=nums[i]-nums[0];
+        int n = nums.size();
+        int ans=0;
+        sort(nums.begin(), nums.end());
+        int sum=0;
+        
+        int low=0;
+        int high=n-1;       
+        int mid = (n)/2;
+        int temp=nums[mid];
+        
+        for(int i=0; i<n; i++){
+            nums[i]=abs(nums[i]-temp);
+        }
+        // for(int i=0; i<n; i++){
+        //     cout<<nums[i]<<endl;
+        // }
+        for(int i=0; i<n; i++){
+            ans+=nums[i];
         }
         return ans;
         
