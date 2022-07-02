8dbb734493b2f048af6d37a6a81f7c2fee58e7c0
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-07-02 17:52:24 +0530

    Time: 68 ms (7.20%), Space: 16.3 MB (8.40%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    bool cycleDFS(int i, vector<int>&visited, vector<int>&DFSvisited, vector<int> adj[]){
+        
+        visited[i]=1;
+        DFSvisited[i]=1;
+        
+        for(auto it:adj[i]){
+            
+            if(visited[it]==0){
+                if(cycleDFS(it, visited, DFSvisited, adj)) return true;
+            }
+            else if (DFSvisited[it]==1){
+                    return true;
+                }           
+        }
+        DFSvisited[i]=0;
+        return false;  
+    }
+
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        vector<int>adj[100001];
+        for(int i=0; i<prerequisites.size(); i++) // convert edge list to adjacency list
+        {
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        } 
+        
+        vector<int>visited(numCourses, 0);
+        vector<int>DFSvisited(numCourses,0);
+        
+        
+        for(int i=0; i<numCourses; i++){
+            
+            if(!visited[i]){
+                if(cycleDFS(i,visited, DFSvisited, adj)) return false;
+            }
+            
+        }
+         return true;
+        
+    }
+};
