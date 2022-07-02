ddb0c2c95e5fd9b2bb5e251abeb9fab78e0ed72f
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-07-02 09:31:44 +0530

    Time: 103 ms (50.50%), Space: 34.5 MB (6.74%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+        
+        long long ans=0;
+        horizontalCuts.push_back(h);
+        horizontalCuts.push_back(0);
+        verticalCuts.push_back(w);
+        verticalCuts.push_back(0);
+        
+        sort(horizontalCuts.begin(), horizontalCuts.end());
+        sort(verticalCuts.begin(), verticalCuts.end());
+        
+        vector<long long>height(horizontalCuts.size(),0);
+        vector<long long>width(verticalCuts.size(),0);
+       
+        long long maxH=0;
+        for(int i=0; i<horizontalCuts.size()-1; i++){
+            height[i]=(horizontalCuts[i+1]%mod-horizontalCuts[i]%mod)%mod;
+            maxH = max(maxH , height[i]);
+            
+        }
+        
+        long long maxW=0;
+        for(int i=0; i<verticalCuts.size()-1; i++){
+            width[i]=(verticalCuts[i+1]%mod-verticalCuts[i]%mod)%mod;
+            maxW = max(maxW , width[i]);
+        }
+        
+        ans=(maxH*maxW)%mod;
+        return ans;   
+    }
+};
