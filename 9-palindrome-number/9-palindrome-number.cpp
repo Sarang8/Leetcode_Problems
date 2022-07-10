c5194ba87717b26d67295440d896be5ba19ac3e7
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-07-10 21:39:19 +0530

    Time: 8 ms (94.75%), Space: 5.9 MB (31.52%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+      long int r,sum=0,t;
+        t=x;
+        while(t>0)
+        {
+            r=t%10;
+            sum=sum*10+r;
+            t/=10;
+        }
+        if(sum==x)
+            return true;
+        return false;
+        
+    }
+};
