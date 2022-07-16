71701b4c4b9025ffd54b8078806a2e7e2ddd8493
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-07-16 17:14:28 +0530

    Time: 7 ms (64.59%), Space: 6.9 MB (45.91%) - LeetHub

---
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        
+        int mod=1e9+7;
+        int n=s.size();
+        vector<int>dp(n+1, 0);
+        unordered_map<char, int>m;
+        dp[0]=1;
+        
+        for(int i=1; i<n+1; i++){
+            
+            char ch=s[i-1];
+            
+            dp[i] = (2*dp[i-1]%mod)%mod;
+            
+            if(m.find(ch)!=m.end()){
+                int p=m[ch];
+                dp[i] = (dp[i]%mod-dp[p-1]%mod)%mod;                
+            }
+            m[ch] = i;            
+        }
+        
+        if(dp[n]<0) dp[n]+=mod;
+        
+        return (dp[n]-1)%mod;
+        
+        
+//         int n=s.length();
+//         int mod=1e9+7;
+        
+//         vector<long >dp(n+1,0);
+       
+//         dp[0]=1; // Empty is also a subsequence
+//         unordered_map<char,int>mp;
+        
+//          for(int i=1;i<dp.size();i++){
+             
+//              char ch=s[i-1]; // bcz indexing in dp is 1 ahead of s
+              
+//               dp[i]=(2*dp[i-1]) % mod; // total subsequences possible till a char
+             
+//              if(mp.find(ch)!=mp.end()){
+                 
+//                  int j=mp[ch]; // index of the repeated character
+                 
+//                  dp[i]=(dp[i]-dp[j-1])%mod; // subtracting the value with 1 index before j  
+//                   // dp[i]=dp[i]%mod;
+//              }
+             
+//              // No need of else already done above
+//              mp[ch]=i; // index of dp array not of the sting s
+                 
+//          }
+//           // these are the important edge cases
+//          if(dp[n]<0) dp[n]+=mod;
+        
+//         return dp[n]-1;
+//        // return dp[n]-1;  // bcz not including the empty susequence
+//     }
+        
+    }
+};
