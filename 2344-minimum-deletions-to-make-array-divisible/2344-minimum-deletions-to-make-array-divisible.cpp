0f9bb427bd5fd117ce5c13ee42a913044f3dd536
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-07-17 11:28:26 +0530

    Time: 261 ms (50.00%), Space: 77.9 MB (25.00%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
@@ -3,28 +3,43 @@ public:
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
         
         int n=nums.size();
-        map<int,int>m;
-        int ans=0;
-        for(int i=0; i<n; i++){
-            m[nums[i]]++;
+        
+        int GCD=numsDivide[0];
+        for(int i=1; i<numsDivide.size();i++){
+            GCD = __gcd(GCD, numsDivide[i]);
         }
         
-        for(auto it:m){
-            
-            int x=it.first;
-            int y=it.second;
-            
-            int i;
-            for(i=0;i<numsDivide.size(); i++){
-                if(numsDivide[i] % x !=0){
-                    ans+=y;
-                    break;
-                }
-            }
-            if(i==numsDivide.size()) break;
-            
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0; i<n; i++){
+            if(nums[i]>GCD) return -1;
+            if(GCD%nums[i]==0) return i;
         }
-        if(ans==n) return -1;
-        return ans;
+        
+        return -1;
+        
+//         int n=nums.size();
+//         map<int,int>m;
+//         int ans=0;
+//         for(int i=0; i<n; i++){
+//             m[nums[i]]++;
+//         }
+        
+//         for(auto it:m){
+            
+//             int x=it.first;
+//             int y=it.second;
+            
+//             int i;
+//             for(i=0;i<numsDivide.size(); i++){
+//                 if(numsDivide[i] % x !=0){
+//                     ans+=y;
+//                     break;
+//                 }
+//             }
+//             if(i==numsDivide.size()) break;
+//         }
+//         if(ans==n) return -1;
+//         return ans;
     }
 };
