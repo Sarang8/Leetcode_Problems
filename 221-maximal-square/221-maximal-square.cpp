b7f30ac33c5a96317c6cd600ea5d32728cf6398d
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-07-22 11:50:01 +0530

    Time: 78 ms (78.91%), Space: 18.3 MB (11.61%) - LeetHub

---
+++ 221-maximal-square/221-maximal-square.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int maxArea=0;
+        vector<vector<int>>dp(m, vector<int>(n,0));
+        int maxi=0;
+        
+        
+        for(int i=0; i<n; i++){
+            dp[0][i] = matrix[0][i]-'0';
+            maxi=max(maxi, dp[0][i]);
+        }
+        for(int i=0; i<m; i++){
+            dp[i][0] = matrix[i][0]-'0';
+            maxi=max(maxi, dp[i][0]);
+        }
+        
+        for(int i=1;i<m; i++){
+            for(int j=1; j<n; j++){ 
+                if(matrix[i][j]=='1'){
+                    dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]));
+                    maxi = max(maxi, dp[i][j]);
+                }  
+            }
+        }
+        
+//         for(int i=0;i<m; i++){
+//             for(int j=0; j<n; j++){ 
+//                 cout<<dp[i][j]<<" ";
+//             }
+//             cout<<endl;
+//         }
+        
+        return maxi*maxi;
+    }
+};
