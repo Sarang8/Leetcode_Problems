effa316fdecb1eb025b2a56c1909bed8cb215c4b
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-07-22 20:46:37 +0530

    Time: 85 ms (74.22%), Space: 48.5 MB (43.19%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int n=nums.size();
+        if(n==1) return true;
+        if(nums[0]==0 && n>1) return false;
+        
+        int step=nums[0];
+        
+        for(int i=1; i<n; i++){
+            
+            step--;
+            
+            if(step<0){
+                return false;
+            }
+            
+            if(nums[i]>step){
+                step=nums[i];
+            }
+            
+        }
+        
+        return true;
+    }
+};
