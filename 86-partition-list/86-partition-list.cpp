9022bc02fb06407fedbf8b4574873c21a5bf3ab4
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-07-22 09:43:54 +0530

    Time: 7 ms (73.71%), Space: 10.3 MB (32.53%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -12,14 +12,14 @@ class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
         
-        ListNode* lesser = new ListNode(0);
-        ListNode* greater = new ListNode(0);
+        ListNode* lesser = new ListNode (0);
+        ListNode* greater = new ListNode (0);
         ListNode* lesserHead=lesser;
         ListNode* greaterHead=greater;
         
-        while(head!=NULL){
+        while(head){
             
-            if(head->val < x){
+            if(head->val<x){
                 lesser->next=head;
                 lesser=lesser->next;
             }
@@ -27,11 +27,12 @@ public:
                 greater->next=head;
                 greater=greater->next;
             }
-            head=head->next;
+            
+            head=head->next;   
         }
+        
         lesser->next=greaterHead->next;
         greater->next=NULL;
         return lesserHead->next;
-        
     }
 };
