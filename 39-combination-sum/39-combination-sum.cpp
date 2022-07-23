ddb6d1a4627fafcd20621a801d5835db1d8e892f
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-07-23 17:26:54 +0530

    Time: 15 ms (53.63%), Space: 11 MB (80.35%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void solve(int ind, int target, vector<int>&ds, vector<vector<int>>&ans, vector<int>& candidates){
+        if(ind==candidates.size()){
+            if(target==0){
+                ans.push_back(ds);
+               
+            }
+             return;
+        }
+        
+        if(candidates[ind]<=target){
+            ds.push_back(candidates[ind]);
+            solve(ind, target-candidates[ind], ds, ans, candidates);
+            ds.pop_back();
+        }
+        
+        solve(ind+1,target,ds, ans, candidates);  
+    }
+    
+    
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        vector<vector<int>>ans;
+        vector<int>ds;
+        
+        solve(0, target, ds, ans, candidates);
+        return ans;
+        
+    }
+};
