d55197bd6f3216ebafd07c70ba4984000cc7b951
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-08-04 22:58:15 +0530

    Time: 55 ms (62.97%), Space: 20 MB (32.78%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>> merged;
+        
+        if(intervals.size()==0){
+            return merged;
+        }
+        
+        sort(intervals.begin(), intervals.end());
+        
+        vector<int>tempInterval = intervals[0];
+        
+        for(auto it : intervals){
+            
+            if(it[0]<=tempInterval[1]){
+                tempInterval[1] = max (tempInterval[1], it[1]);        
+            }
+            else{
+                merged.push_back(tempInterval);
+                tempInterval = it;
+            }   
+        }
+        merged.push_back(tempInterval);
+        return merged;  
+    }
+};
