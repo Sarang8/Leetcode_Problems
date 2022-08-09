362cd88099183bf87df037c061aa90f78faf5c40
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-08-09 22:03:40 +0530

    Time: 14 ms (61.22%), Space: 9.5 MB (64.80%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,38 +1,72 @@
 class Solution {
 public:
-    bool solve(string s, unordered_set<string>&dictionry){
+    
+//     bool solve(string s, unordered_set<string>&dictionry){
         
-        if(dictionry.size()==0) return false;
+//         if(dictionry.size()==0) return false;
         
-        vector<bool> dp(s.size()+1,false);
-        dp[0]=true;
+//         vector<bool> dp(s.size()+1,false);
+//         dp[0]=true;
         
-        for(int i=1;i<=s.size();i++)
-        {
-            for(int j=i-1;j>=0;j--)
-            {
-                if(dp[j]==true)
-                {
-                    string word = s.substr(j,i-j);
-                    if(dictionry.find(word)!= dictionry.end())
-                    {
-                        dp[i]=true;
-                        break; 
-                    }
-                }
+//         for(int i=1;i<=s.size();i++)
+//         {
+//             for(int j=i-1;j>=0;j--)
+//             {
+//                 if(dp[j]==true)
+//                 {
+//                     string word = s.substr(j,i-j);
+//                     if(dictionry.find(word)!= dictionry.end())
+//                     {
+//                         dp[i]=true;
+//                         break; 
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return dp[s.size()];    
+//     }
+    
+    
+    int dp[301];
+    
+    
+    int solve(int ind, string s,unordered_set<string>&dictionry){
+        
+        if(ind==s.size()){
+            return 1;
+        }
+        
+        if(dp[ind]!=-1) return dp[ind];
+        
+        
+        string temp="";
+        for(int i=ind; i<s.size(); i++){
+            
+            temp += s[i];
+            if(dictionry.find(temp)!=dictionry.end()){
+                if(solve(i+1, s, dictionry))
+                    return dp[ind]=1;
             }
         }
         
-        return dp[s.size()];    
+        
+        return dp[ind]=0;
     }
     
-    bool wordBreak(string s, vector<string>& wordDict) {
+    
+    
+    bool wordBreak(string s, vector<string> wordDict) {
         
-        unordered_set<string>dictionry;
-        for(int i=0; i<wordDict.size(); i++){
-            dictionry.insert(wordDict[i]);
-        }
-        
-        return solve(s,dictionry);
+        // unordered_set<string>dictionry;
+        // for(int i=0; i<wordDict.size(); i++){
+        //     dictionry.insert(wordDict[i]);
+        // }
+        unordered_set<string> dictionry(wordDict.begin(), wordDict.end());
+        memset(dp,-1,sizeof(dp));
+        return solve(0,s,dictionry);
     }
+    
+    
+
 };
