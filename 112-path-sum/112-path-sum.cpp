2cc9d3807362e51074a5f83ac0c8ba7356ec3409
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-08-09 12:20:10 +0530

    Time: 15 ms (68.43%), Space: 21.3 MB (39.41%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -11,18 +11,36 @@
  */
 class Solution {
 public:
-    
-    
-    
-    
-    
-    bool hasPathSum(TreeNode* root, int sum) {
+    bool solve(TreeNode* root, int targetSum, int sum){
         
-
-        if(!root)return false;                                         //Terminating Condition
-        sum=sum-root->val;                                             //Body
-        if(sum==0&&!root->left&&!root->right)return true;              //body
-        return hasPathSum(root->left,sum)||hasPathSum(root->right,sum);//Propagation
+        sum+=root->val;
+        
+        if(sum==targetSum && root->left==NULL && root->right==NULL){
+            return true;
+        }
+        
+        
+        bool t1=0, t2=0;
+        if(root->left){
+           t1=solve(root->left, targetSum, sum); 
+        }
+        if(root->right){
+           t2=solve(root->right, targetSum, sum); 
+        }
+        
+        return  t1 or t2; 
+        
+        
+        
+    }
+    
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        
+        if(root==NULL) return false;
+        
+        return solve(root, targetSum, 0);
+        
+        
         
     }
 };
