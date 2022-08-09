ff5e2cced5f4cb74160a6797449872c4c47561ed
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-08-09 14:38:17 +0530

    Time: 37 ms (62.25%), Space: 27.5 MB (99.57%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -12,59 +12,30 @@
 class Solution {
 public:
     
-//     int height(TreeNode* root, int &maxi){
-        
-//         if(root==NULL){
-//             return 0;
-//         }
-        
-//         //We USED MAX(0, HEIGHT) BECAUSE IF IN CASE WE GET NEGATIVE SUM THEN WE WILL NOT CONSIDER IT AND MAKE IT AS 0.
-//         int lh = max(0,height(root->left, maxi));
-//         int rh = max(0,height(root->right, maxi));
-        
-//         maxi = max(maxi, root->val+lh+rh);
-        
-//         return (root->val) + max(lh,rh);
-//         //HERE MAX(LH,RH) IS NECEESARY TO KNOW WHEHER U TAKE LRFT PATH OR RIGHT PATH
-//     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     int solve(TreeNode* root, int &maxi){
+        
+        if(root==NULL) return 0;
+        
+        int lefti = max(0,solve(root->left, maxi));
+        int righti = max(0, solve(root->right, maxi));
+        
+        maxi=max(maxi, root->val+lefti+righti);
+                       
+        return root->val+max(lefti, righti);
+    
+    }
+    
+    
+    
+    int maxPathSum(TreeNode* root) {
         
         if(root==NULL){
             return 0;
         }
         
+        int maxi=INT_MIN;
+        solve(root,maxi);
         
-        int left = max(0,solve(root->left, maxi));
-        int right = max(0, solve(root->right,maxi));
-        
-        maxi = max(maxi, (root->val + left+right));
-        
-        return root->val+max(left,right);
-    
-    }    
-    
-    int maxPathSum(TreeNode* root) {
-        
-        int maxi = INT_MIN;
-        solve(root, maxi);
         return maxi;
         
     }
