d791ee790c6f05eb6d441d5b96ef814cfab47399
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-08-09 15:22:59 +0530

    Time: 2994 ms (5.00%), Space: 51.2 MB (25.91%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -2,65 +2,34 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         
-//         int n=nums.size();
-        
-//         if(n==0){
-//             return 0;
-//         }
-        
-//         set<int>s;
-        
-//         for(int it:nums){
-//             s.insert(it);
-//         }
-        
-//         int longestStreak=0;
-//         for(int it:nums){
-            
-//             if(s.count(it-1)==0){
-//                 int currStreak=1;
-//                 int currNum = it;
-                
-//                 while(s.count(currNum+1)){
-//                     currStreak++;
-//                     currNum=currNum+1;
-//                 }
-                
-//                 longestStreak=max(longestStreak, currStreak);
-//             }  
-            
-//         }
-        
-        
-//         return longestStreak;
-        
-         if(nums.size()==0){
+        int n=nums.size();
+        if(nums.size()==0){
             return 0;
         }
-        
-        unordered_set<int>s;
-        
-        for(int n:nums){
-            s.insert(n);
+        set<int>s;
+        for(auto it:nums){
+            s.insert(it);
         }
         
-        int longestStreak=0;
+        int longestStreak=1;
         
-        for(int n: nums){
-            if(s.count(n-1)==0){
+        for(int i=0; i<n; i++){
+            
+            if(s.count(nums[i]-1)==0){
                 int currStreak=1;
-                int currNum = n;
+                int currNum=nums[i];
                 
                 while(s.count(currNum+1)){
                     currStreak++;
-                    currNum = currNum+1; 
+                    currNum++;
                 }
-                
-                longestStreak = max(longestStreak, currStreak);
-            }  
+                longestStreak=max(longestStreak, currStreak);
+            }
+            
             
         }
-        return longestStreak;
         
+        
+        return longestStreak;        
     }
 };
