afff0ff71c9f7bbecf05e4846fe4b67afcc9ea51
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-08-14 22:59:57 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (33.18%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -2,43 +2,43 @@ class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
         
-//         sort(stones.begin(), stones.end());
-//         int n=stones.size();
+        sort(stones.begin(), stones.end());
+        int n=stones.size();
         
-//         while(stones.size()>1){
+        while(stones.size()>1){
             
-//             int a=stones[stones.size()-1];
-//             int b=stones[stones.size()-2];
+            int a=stones[stones.size()-1];
+            int b=stones[stones.size()-2];
             
-//             if(a==b){
-//                 stones.pop_back();
-//                 stones.pop_back();
-//             }
-//             else if(a>b){
-//                 stones.pop_back();
-//                 stones.pop_back();
-//                 stones.push_back(a-b);
-//                 sort(stones.begin(), stones.end());
-//             }   
-            
-//         }
-        
-//         if(stones.size()==1)
-//             return stones[0];
-//         else return 0;
-        
-        
-            priority_queue<int> pq(stones.begin(),stones.end());
-    
-            while(pq.size()>1)
-            {
-                int y=pq.top();
-                pq.pop();
-                int x=pq.top();
-                pq.pop();
-                if(x!=y)pq.push(y-x);
+            if(a==b){
+                stones.pop_back();
+                stones.pop_back();
             }
-            return pq.empty()?0:pq.top();
+            else if(a>b){
+                stones.pop_back();
+                stones.pop_back();
+                stones.push_back(a-b);
+                sort(stones.begin(), stones.end());
+            }   
+            
+        }
+        
+        if(stones.size()==1)
+            return stones[0];
+        else return 0;
+        
+        
+//             priority_queue<int> pq(stones.begin(),stones.end());
+    
+//             while(pq.size()>1)
+//             {
+//                 int y=pq.top();
+//                 pq.pop();
+//                 int x=pq.top();
+//                 pq.pop();
+//                 if(x!=y)pq.push(y-x);
+//             }
+//             return pq.empty()?0:pq.top();
         
     }
 };
