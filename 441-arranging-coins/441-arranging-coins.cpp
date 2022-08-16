8faa1e2a1fd70832a13b0196861e00affa67bc38
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-08-16 22:43:59 +0530

    Time: 5 ms (62.53%), Space: 5.8 MB (95.50%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long low = 0;
+        long long high = n;
+     
+        while(low<=high){
+            
+            long long mid = low + (high-low)/2;
+            
+            long long temp = mid*(mid+1)/2;
+            
+            if(temp==n)  return mid;
+            
+            else if(temp>n)  high = mid-1;
+            
+            else if(temp<n) {
+                low= mid+1;  
+            } 
+            
+        }
+        return low-1;
+        
+    }
+};
