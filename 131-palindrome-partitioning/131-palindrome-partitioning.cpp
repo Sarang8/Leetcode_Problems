3fa4c834300567f8a20607c61ed78f4e37f277a6
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-08-18 23:06:52 +0530

    Time: 280 ms (30.02%), Space: 57.6 MB (61.78%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    bool isPalindrome(string s, int low, int high){
+        
+        while(low<=high){
+            if(s[low]!=s[high]){
+                return false;
+            }
+            low++;
+            high--;
+        }
+        return true;
+    }
+    
+    void solve(int ind, string s, vector<string>&ds, vector<vector<string>>&ans){
+        
+        if(ind==s.size()){
+            ans.push_back(ds);
+            return;
+        }
+        
+        
+        for(int i=ind; i<s.size(); i++){
+            
+            if(isPalindrome(s, ind, i)){
+                
+                ds.push_back(s.substr(ind, i-ind+1));
+                solve(i+1, s, ds, ans);
+                ds.pop_back();
+            }
+        }
+    }
+    
+    
+    
+    
+    vector<vector<string>> partition(string s) {
+        
+        vector<vector<string>>ans;
+        vector<string>ds;
+        
+        solve(0,s, ds, ans );
+        return ans;
+        
+        
+        
+        
+        
+    }
+};
