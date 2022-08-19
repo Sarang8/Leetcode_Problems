92e2a5dc2717eda918a72ebd731bc691cbcf42fc
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-08-19 19:22:29 +0530

    Time: 3 ms (83.83%), Space: 10.8 MB (92.65%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
@@ -14,9 +14,9 @@ public:
     
     bool isCousins(TreeNode* root, int x, int y) {
         
-        if(root->val==x && root->val==y) return false;
+        if(root->val==x or root->val==y) return false;
         
-        queue<pair<TreeNode* , TreeNode*>>q;
+        queue<pair<TreeNode*, TreeNode*>>q;
         
         q.push({root, NULL});
         
@@ -24,39 +24,29 @@ public:
             
             TreeNode* xParent=NULL;
             TreeNode* yParent=NULL;
-            
             int size=q.size();
             
-            for(int i=0; i<size; i++){
-                
+            for(int i=0; i<size;i++){
                 TreeNode* node=q.front().first;
-                TreeNode* parent = q.front().second;
+                TreeNode* parent=q.front().second;
                 q.pop();
                 
                 if(node->val==x){
                     xParent = parent;
                 }
-                
-                else if(node->val==y){
-                    yParent=parent;
+                if(node->val==y){
+                    yParent = parent;
                 }
                 
-                
                 if(node->left) q.push({node->left, node});
-                if(node->right) q.push({node->right, node});
-                
+                if(node->right) q.push({node->right, node}); 
             }
             
-            if( xParent || yParent){
-                return xParent && yParent && xParent!=yParent; 
+            if(xParent!=NULL && yParent!=NULL && xParent!=yParent){
+                return true;
             }
-            
-            
         }
-        
-        
         return false;
-        
         
     }
 };
