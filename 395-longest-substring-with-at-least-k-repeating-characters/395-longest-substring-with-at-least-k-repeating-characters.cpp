2ba076772695b22006965b644583444f0b714489
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-08-29 15:24:20 +0530

    Time: 1078 ms (18.57%), Space: 58.5 MB (15.55%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-   
     int longestSubstring(string s, int k) {
         
-        int n = s.size();
-        if ((n == 0) && (k > n))
-            return 0;
+        int n=s.size();
         
-        unordered_map<char, int> counter;
-        for (auto letter : s)
-            counter[letter]++;
+        if(n==0 or k>n){
+            return 0;
+        }
+        
+        unordered_map<char,int>m;
+        
+        for(auto it:s){
+            m[it]++;
+        }
         
         int sub1, sub2;
-        
-        for (int i=0; i<n; i++) {
-            if (counter[s[i]] < k) {
-                sub1 = longestSubstring(s.substr(0, i), k);
-                sub2 = longestSubstring(s.substr(i+1), k);
+        for(int i=0; i<n; i++){
+            
+            if(m[s[i]] < k){
+                sub1 = longestSubstring(s.substr(0,i), k);
+                sub2 = longestSubstring(s.substr(i+1),k);
                 break;
             }
-            if (i == n-1)
+            
+            if(i==n-1){
                 return n;
+            }
         }
         
         return max(sub1, sub2);
-        
-        
     }
 };
