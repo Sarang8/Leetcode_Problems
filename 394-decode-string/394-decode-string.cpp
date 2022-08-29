9a9e63ff87ac66695c257b09aa2a7bf53d6cbbd8
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-08-29 14:48:53 +0530

    Time: 6 ms (19.58%), Space: 11.1 MB (12.13%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -6,41 +6,42 @@ public:
         stack<char>st;
         
         for(int i=0; i<n; i++){
-            
-            if(s[i] != ']'){
+            if(s[i]!=']'){
                 st.push(s[i]);
             }
             
             else{
                 string temp="";
-                while(st.top()!='['){
-                    temp = st.top() + temp;
+                
+                while(st.size()>0 && st.top()!='['){
+                    temp = st.top()+temp;
                     st.pop();
                 }
+                
                 st.pop();
                 
-                string num = "";
+                string num="";
                 while(st.size()>0 && isdigit(st.top())){
-                    num = st.top() + num;
+                    num = st.top()+num;
                     st.pop();
                 }
                 
                 int kTimes = stoi(num);
-                //string temp2=""
-                while(kTimes){
-                    for(int t=0; t<temp.size(); t++){
+                
+                while(kTimes>0){
+                    for(int t=0;t<temp.size();t++){
                         st.push(temp[t]);
                     }
                     kTimes--;
-                }  
-            }  
+                }
+            }
         }
         
         string ans="";
         while(!st.empty()){
             ans = st.top()+ans;
             st.pop();
-        }
+        }        
         return ans;
     }
 };
