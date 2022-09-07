c662b36fc9c56dd23d2d6d01c2bf1af7413e86f7
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-09-07 13:59:05 +0530

    Time: 887 ms (23.79%), Space: 59.9 MB (9.77%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -4,22 +4,23 @@ public:
         
         int n=s.size();
         
-        if(n==0 or k>n){
+        if(n==0 && k>0){
             return 0;
         }
         
-        unordered_map<char,int>m;
+        unordered_map<char, int>m;
         
         for(auto it:s){
             m[it]++;
         }
         
-        int sub1, sub2;
+        int sub1=0,sub2=0;
+        
         for(int i=0; i<n; i++){
             
-            if(m[s[i]] < k){
-                sub1 = longestSubstring(s.substr(0,i), k);
-                sub2 = longestSubstring(s.substr(i+1),k);
+            if(m[s[i]]<k){
+                sub1 = longestSubstring(s.substr(0, i), k);
+                sub2 = longestSubstring(s.substr(i+1), k);
                 break;
             }
             
@@ -28,6 +29,9 @@ public:
             }
         }
         
+        
         return max(sub1, sub2);
+        
+        
     }
 };
