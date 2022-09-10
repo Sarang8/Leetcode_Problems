0b7b60329dcf21e5deea543720769099dd8603a8
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-09-10 16:24:03 +0530

    Time: 28 ms (14.67%), Space: 11.9 MB (89.56%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -2,45 +2,40 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         
-        stack<int>st;
         int n=tokens.size();
-        
-        //ONLY PROBLEM CAN COME WHEN '-' && '/' SIGN COME BECAUSE YOU HAVE TO TAKE CARE OF B-A ANS B/A
+        stack<int>st;
         
         for(int i=0; i<n; i++){
-            string ch=tokens[i];
             
-            if(tokens[i]=="+" or tokens[i]=="-" or tokens[i]=="*" or tokens[i]=="/"){
+            if(tokens[i]=="+" or tokens[i]=="-" or tokens[i]=="/" or tokens[i]=="*"){
                 
+                int temp=0;
                 int a=st.top();
                 st.pop();
                 int b=st.top();
                 st.pop();
-                int temp=0;
+                
                 
                 if(tokens[i]=="+"){
-                    temp = b+a;
+                    temp=b+a;
                 }
                 else if(tokens[i]=="-"){
-                    temp=b-a;             //CAREFULLY LOOK THAT IT WILL BE B-A 
+                    temp=b-a;
                 }
                 else if(tokens[i]=="*"){
                     temp=b*a;
                 }
                 else if(tokens[i]=="/"){
-                    temp=b/a;          //CAREFULLY LOOK THAT IT WILL BE B/A
+                    temp=b/a;
                 }
                 st.push(temp);
             }
             
             else{
-                st.push(stoi(tokens[i]));
-            } 
-            
+                st.push(stoi(tokens[i]));                
+            }
+           
         }
-        
-        
-        return st.top();
-        
+        return st.top();    
     }
 };
