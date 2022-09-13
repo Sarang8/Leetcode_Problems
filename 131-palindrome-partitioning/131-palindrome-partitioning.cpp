1437e84af9f6448ce340c8aeed6036e501d6afee
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-09-13 15:16:42 +0530

    Time: 204 ms (57.09%), Space: 53.5 MB (73.61%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    
-    bool isPalindrome(string s, int low, int high){
+    bool isPal(string &s, int low, int high){
         
         while(low<=high){
             if(s[low]!=s[high]){
@@ -11,40 +10,36 @@ public:
             high--;
         }
         return true;
+        
     }
+    
     
     void solve(int ind, string s, vector<string>&ds, vector<vector<string>>&ans){
         
-        if(ind==s.size()){
+        if(ind>=s.size()){
             ans.push_back(ds);
             return;
         }
         
         
-        for(int i=ind; i<s.size(); i++){
-            
-            if(isPalindrome(s, ind, i)){
+        for(int i=ind;i<s.size();i++){
+            if(isPal(s, ind, i)){
                 ds.push_back(s.substr(ind, i-ind+1));
                 solve(i+1, s, ds, ans);
                 ds.pop_back();
             }
         }
+        
+        
     }
-    
-    
     
     
     vector<vector<string>> partition(string s) {
         
-        vector<vector<string>>ans;
+        int n=s.size();
         vector<string>ds;
-        
-        solve(0,s, ds, ans );
-        return ans;
-        
-        
-        
-        
-        
+        vector<vector<string>>ans;
+        solve(0, s, ds, ans);
+        return ans;   
     }
 };
