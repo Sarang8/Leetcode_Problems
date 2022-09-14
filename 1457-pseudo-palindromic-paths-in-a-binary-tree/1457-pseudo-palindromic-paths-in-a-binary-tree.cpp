47bdd6775daeeb592e3b21c50b9c102990b6fe8d
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-09-14 09:03:27 +0530

    Time: 784 ms (25.77%), Space: 192.2 MB (37.56%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int ,int>mp;
+    int ans=0;
+    
+    void solve(TreeNode* root){
+        
+        if(root==NULL){
+            return;
+        }
+        
+        mp[root->val]++;
+        
+        if(root->left==NULL && root->right==NULL){
+            int odd=0;
+            
+            for(auto it:mp){
+                if(it.second%2 != 0){
+                    odd++;
+                }
+            }
+            
+            if(odd<=1){
+                ans++;
+            }
+        }
+        
+        solve(root->left);
+        solve(root->right);  
+        mp[root->val]--;
+    }
+    
+    
+    
+    int pseudoPalindromicPaths (TreeNode* root) {
+        
+        
+        solve(root);
+        return ans;
+        
+    }
+};
