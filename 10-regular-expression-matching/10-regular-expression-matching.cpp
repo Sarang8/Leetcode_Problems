3b1b6d7b92a5309b52ca35827e1972d2752b4695
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-10-07 15:36:29 +0530

    Time: 18 ms (36.81%), Space: 21.3 MB (8.42%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
-   
-    bool solve(int i, int j, string &s, string &p, vector<vector<int>>&dp){
+    bool solve(int i,int j, string s, string p, vector<vector<int>>&dp){
         
+        int n=s.size();
+        int m=p.size();
         
         if(i>=s.size() && j>=p.size()){
             return true;
         }
-        
         if(j>=p.size()){
             return false;
         }
-        
+       
         if(dp[i][j]!=-1) return dp[i][j];
+       
         
-        bool match = (i<s.size() && (s[i]==p[j] or p[j]=='.'));
+        bool match=0;
         
-        if( j+1 < p.size() &&  p[j+1]=='*'){
-            
-            return dp[i][j] =   solve(i, j+2, s, p, dp) or    //NOT TAKE STAR
-                                (match && solve(i+1, j,s,p,dp));       //STAR TAKEN
-            
+        if(i<s.size() && (s[i]==p[j] or p[j]=='.')){
+            match=1;
         }
         
-        if(match==true){
-            return dp[i][j]=solve(i+1, j+1,s,p,dp);
+        if(j+1<m && p[j+1]=='*'){
+            return dp[i][j]=solve(i, j+2, s,p, dp) or (match && solve(i+1, j, s, p, dp));
         }
         
+        if(match){
+            return dp[i][j]=solve(i+1, j+1, s, p, dp);
+        }
+            
         return false;
-        
     }
     
     
-    
     bool isMatch(string s, string p) {
-        
+                
         int n=s.size();
         int m=p.size();
-        //memset(dp,-1,sizeof(dp));
-        
         vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
-        
-        return solve(0, 0, s, p, dp);
-        
+        return solve(0, 0,s, p, dp);   
     }
 };
