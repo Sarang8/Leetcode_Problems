af92b7d6d9f916c7dee0d7dd21ff3c5a58517bee
Sarang8 <101692525+Sarang8@users.noreply.github.com>
2022-10-14 09:32:26 +0530

    Time: 2213 ms (18.50%), Space: 294.6 MB (88.09%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -12,36 +12,53 @@ class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
         
-        ListNode* slow=head;
-        ListNode* fast=head;
-        ListNode* pre=new ListNode(0);
-        pre->next=slow;
+//         ListNode* slow=head;
+//         ListNode* fast=head;
+//         ListNode* pre=new ListNode(0);
+//         pre->next=slow;
+        
+//         if(head==NULL or head->next==NULL){
+//             return NULL;
+//         }
+        
+//         int len=0;
+//         ListNode* temp=head;
+//         while(temp){
+//             len++;
+//             temp=temp->next;
+//         }
+        
+//         while(fast->next && fast->next->next){
+            
+//             slow=slow->next;
+//             pre=pre->next;
+//             fast=fast->next->next;
+//         }
+        
+//         if(len%2==0){
+//             slow->next=slow->next->next;
+//         }
+//         else{
+//             pre->next=slow->next;
+//         }
+        
+//         return head;
         
         if(head==NULL or head->next==NULL){
             return NULL;
         }
         
-        int len=0;
-        ListNode* temp=head;
-        while(temp){
-            len++;
-            temp=temp->next;
-        }
+        ListNode* slow=head;
+        ListNode* fast=head;
+        ListNode* pre;
         
-        while(fast->next && fast->next->next){
-            
+        while(fast && fast->next){
+            pre=slow;
             slow=slow->next;
-            pre=pre->next;
             fast=fast->next->next;
         }
         
-        if(len%2==0){
-            slow->next=slow->next->next;
-        }
-        else{
-            pre->next=slow->next;
-        }
-        
+        pre->next=slow->next;
         return head;
         
         
